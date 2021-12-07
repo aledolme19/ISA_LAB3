0f06f933874ec3d355b9e64ffec43ee475a22d74
aledolme19 <s285624@studenti.polito.it>
2021-12-07 08:52:16 +0100

    RISC_V

---
+++ src/IMM_UNIT.vhd
@@ -0,0 +1,21 @@
+---------------------------------------------------------------------------------------------
+-- File: IMM_UNIT.vhd
+---------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity IMM_UNIT is
+    port(
+ 
+    );
+end entity IMM_UNIT;
+
+architecture BEHAVIORAL of IMM_UNIT is
+
+begin
+
+
+
+end BEHAVIORAL;
---
+++ src/RISC_V.vhd
@@ -0,0 +1,21 @@
+---------------------------------------------------------------------------------------------
+-- File: RISC_V.vhd
+---------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity RISC_V is
+    port(
+ 
+    );
+end entity RISC_V;
+
+architecture BEHAVIORAL of RISC_V is
+
+begin
+
+
+
+end BEHAVIORAL;
