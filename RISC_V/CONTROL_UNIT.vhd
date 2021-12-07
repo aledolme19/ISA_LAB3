4efc956b6ab9bc9252a4ef9db5d247b6aa6fd7aa
aledolme19 <s285624@studenti.polito.it>
2021-12-07 08:47:21 +0100

    RISC_V

---
+++ RISC_V/CONTROL_UNIT.vhd
@@ -0,0 +1,21 @@
+---------------------------------------------------------------------------------------------
+-- File: IMM_UNIT.vhd
+---------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity CONTROL_UNIT is
+    port(
+ 
+    );
+end entity CONTROL_UNIT;
+
+architecture BEHAVIORAL of CONTROL_UNIT is
+
+begin
+
+
+
+end BEHAVIORAL;
