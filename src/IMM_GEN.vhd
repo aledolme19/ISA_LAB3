2902ff68c931a2f463f070dca52d0122cb1a07d6
francescobabbaro <francesco.babbaro@gmail.com>
2021-12-29 13:32:54 +0100

    Changed imm_gen output on 32 bits

--- src/IMM_GEN.vhd
+++ src/IMM_GEN.vhd
@@ -9,7 +9,7 @@ use ieee.numeric_std.all;
 entity IMM_GEN is
 	port(
 		IMM_GEN_IN_INSTR : in  std_logic_vector(31 downto 0);
-		IMM_GEN_OUT      : out std_logic_vector(63 downto 0));
+		IMM_GEN_OUT      : out std_logic_vector(31 downto 0));
 end entity IMM_GEN;
 
 architecture BEHAVIORAL of IMM_GEN is
@@ -30,33 +30,33 @@ begin
 		case CONCATENATION is
 			-- ADDI: sign extension
 			when "0010011" & "000" =>
-				IMM_GEN_OUT <= (63 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 20);
+				IMM_GEN_OUT <= (31 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 20);
 			-- ANDI: sign extension
 			when "0010011" & "111" =>
-				IMM_GEN_OUT <= (63 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 20);
+				IMM_GEN_OUT <= (31 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 20);
 			-- SRAI: 5 LSBs of the immediate, no sign extension
 			when "0010011" & "101" =>
-				IMM_GEN_OUT <= (63 downto 5 => '0') & IMM_GEN_IN_INSTR(24 downto 20);
+				IMM_GEN_OUT <= (31 downto 5 => '0') & IMM_GEN_IN_INSTR(24 downto 20);
 			-- BEQ: immediate has to be assembled in a particular way + sign extension
 			when "1100011" & "000" =>
-				IMM_GEN_OUT <= (63 downto 13 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31) & IMM_GEN_IN_INSTR(7) & IMM_GEN_IN_INSTR(30 downto 25) & IMM_GEN_IN_INSTR(11 downto 8) & '0';
+				IMM_GEN_OUT <= (31 downto 13 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31) & IMM_GEN_IN_INSTR(7) & IMM_GEN_IN_INSTR(30 downto 25) & IMM_GEN_IN_INSTR(11 downto 8) & '0';
 			-- LW: sign extension
 			when "0000011" & "010" =>
-				IMM_GEN_OUT <= (63 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 20);
+				IMM_GEN_OUT <= (31 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 20);
 			-- SW: immediate has to be assembled in a particular way + sign extension
 			when "0100011" & "010" =>
-				IMM_GEN_OUT <= (63 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 25) & IMM_GEN_IN_INSTR(11 downto 7);
+				IMM_GEN_OUT <= (31 downto 12 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 25) & IMM_GEN_IN_INSTR(11 downto 7);
 			-- AUIPC
 			when "0010111" & "---" =>
-				IMM_GEN_OUT <= (63 downto 32 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 12) & (11 downto 0 => '0');
+				IMM_GEN_OUT <= IMM_GEN_IN_INSTR(31 downto 12) & (11 downto 0 => '0');
 			-- LUI
 			when "0110111" & "---" =>
-				IMM_GEN_OUT <= (63 downto 32 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31 downto 12) & (11 downto 0 => '0');
+				IMM_GEN_OUT <= IMM_GEN_IN_INSTR(31 downto 12) & (11 downto 0 => '0');
 			-- JAL
 			when "1101111" & "---" =>
-				IMM_GEN_OUT <= (63 downto 21 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31) & IMM_GEN_IN_INSTR(19 downto 12) & IMM_GEN_IN_INSTR(20) & IMM_GEN_IN_INSTR(30 downto 21) & '0';
+				IMM_GEN_OUT <= (31 downto 21 => IMM_GEN_IN_INSTR(31)) & IMM_GEN_IN_INSTR(31) & IMM_GEN_IN_INSTR(19 downto 12) & IMM_GEN_IN_INSTR(20) & IMM_GEN_IN_INSTR(30 downto 21) & '0';
 			when others =>
-				IMM_GEN_OUT <= (63 downto 0 => '-');
+				IMM_GEN_OUT <= (31 downto 0 => '-');
 		end case;
 	end process case_process;
 
